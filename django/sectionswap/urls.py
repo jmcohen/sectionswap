09c8e47ee6a208f3d53c1d84c5c8d09878d638b7
Jeremy Cohen <jmcohen@princeton.edu>
2013-03-29 17:23:43 -0400

    Made the default url pattern call the swap.views.index view

--- django/sectionswap/urls.py
+++ django/sectionswap/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+import swap
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^', 'swap.views.index'),
     # Examples:
     # url(r'^$', 'sectionswap.views.home', name='home'),
     # url(r'^sectionswap/', include('sectionswap.foo.urls')),
