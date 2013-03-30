b46c9962b46e529e63652d81d61886c83c4080f8
Jeremy Cohen <jmcohen@princeton.edu>
2013-03-30 00:55:05 -0400

    changed models

--- django/swap/models.py
+++ django/swap/models.py
@@ -1,49 +1,57 @@
 from django.db import models
 
 class Course(models.Model):
-    code = models.CharField(max_length=10)
-    name = models.CharField(max_length=128)
-    hits = models.IntegerField(default=0) # times requested; for diagnostics
+	code = models.CharField(max_length=10)
+	name = models.CharField(max_length=128)
+	number = models.CharField(max_length=30, primary_key=True)
+	hits = models.IntegerField(default=0) # times requested; for diagnostics
     # section_set (ForeignKey)
 
-    def __eq__(self, other):
-    	if other:
-    		return self.code == other.code
-    	return None
-
+	def __eq__(self, other):
+		if other:
+			return self.number == other.number
+		return None
+		
 	def __ne__(self, other):
 		if other:
 			return not self.__eq__(other)
 		return None
-
-    def __unicode__(self):
-        return self.short_name() + ' | ' + self.name
-
-    def short_name(self):
-        return self.code
-
+	
+	def __unicode__(self):
+		return self.short_name() + ' | ' + self.name
+        
+	def short_name(self):
+		return self.number
+		
 class Section(models.Model):
-    course = models.ForeignKey(Course)
-    name   = models.CharField(max_length=4)
-    days   = models.CharField(max_length=15)
-    time   = models.CharField(max_length=19)
-    # TODO: room? teacher?
-    students = models.IntegerField(default=0)
-    seats    = models.IntegerField(default=0)
+	course = models.ForeignKey(Course)
+	name   = models.CharField(max_length=4)
+	days   = models.CharField(max_length=15)
+	time   = models.CharField(max_length=19)
+	# TODO: room? teacher?
+	max = models.IntegerField(default=1000)
+	enroll = models.IntegerField(default=0)
+	isClosed = models.BooleanField(default=False)
      
     # TODO: use something less expensive than str compare
-    def __eq__(self, other):
-        return type(self) == type(other) \
-            and unicode(self) == unicode(other)
-    
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __unicode__(self):
-        return self.course.short_name() + ' ' + self.name
-
-    def sorted_had_by_set(self):
-        return sorted(self.had_by_set.all())
+	def __eq__(self, other):
+		return type(self) == type(other) \
+			and unicode(self) == unicode(other)
+	
+	def __ne__(self, other):
+		return not self.__eq__(other)
+ 
+	def __unicode__(self):
+		return self.course.short_name() + ' ' + self.name
+		
+	def sorted_had_by_set(self):
+		return sorted(self.had_by_set.all())
+		
+class Entry(models.Model):
+	courseNumber = models.CharField(max_length=30)
+	section = models.CharField(max_length=30)
+	totalEnroll = models.IntegerField(default=0)
+	totalClosed = models.BooleanField(default=False)	
 
 class User(models.Model):
     netid = models.CharField(max_length=8)
--- reference/update.py
+++ reference/update.py
@@ -3,16 +3,14 @@ from django.core.management import setup_environ
 setup_environ(princetonpounce.settings)
 
 from pounce.models import Course, Class, Subscription, Entry, CoursesList
-import pounce.log as log
-from django.core.mail import EmailMessage
+# import pounce.log as log
 import urllib2
 from bs4 import BeautifulSoup
 
 def updateCourse(course):
-	log.log("Updating %s" % str(course))
+# 	log.log("Updating %s" % str(course))
 		
- 	url = "https://registrar.princeton.edu/course-offerings/course_details.xml?courseid={}&term=1134".format(course.number)
-# 	url = "http://princetonpounce.com/course-offerings/course_details.xml?courseid={}&term=1134".format(course.number)
+ 	url = "https://registrar.princeton.edu/course-offerings/course_details.xml?courseid={}&term=1142".format(course.number)
 	html = urllib2.urlopen(url).read()
 	soup = BeautifulSoup(html)
 	
@@ -23,35 +21,35 @@ def updateCourse(course):
 			classNumber = fields[0].get_text().strip()
 			if not classNumber:
 				continue
+			print classNumber
 				
-			theclass, created = Class.objects.get_or_create(number=classNumber, course=course)
-			theclass.title = fields[1].get_text().strip()
-			theclass.time = fields[2].get_text().strip().replace("\n", "")
-			theclass.days = fields[3].get_text().strip().replace("\n", "")
-			raw = fields[5].get_text().replace("\n","").replace(" ","")
-			sindex = raw.find("Enrolled:")
-			eindex = raw.find("Limit:")
-			theclass.enroll = int(raw[sindex+9:eindex])
-			theclass.max = int(raw[eindex+6:] or 1000)
-			
-			isClosed = fields[6].get_text().strip() == "Closed"
-			if not isClosed:
-				log.log("Class %s is now open!" % str(classNumber))
-				for subscription in Subscription.objects.filter(theclass=theclass, active=True):
-					log.log("Sending subscription %s." % str(subscription))
-					subscription.sendNotification()
-					subscription.active = False
-					subscription.save()
-					
-			theclass.isClosed = isClosed
-			
-			theclass.save()
+# 			theclass, created = Class.objects.get_or_create(number=classNumber, course=course)
+# 			theclass.title = fields[1].get_text().strip()
+# 			theclass.time = fields[2].get_text().strip().replace("\n", "")
+# 			theclass.days = fields[3].get_text().strip().replace("\n", "")
+# 			raw = fields[5].get_text().replace("\n","").replace(" ","")
+# 			sindex = raw.find("Enrolled:")
+# 			eindex = raw.find("Limit:")
+# 			theclass.enroll = int(raw[sindex+9:eindex])
+# 			theclass.max = int(raw[eindex+6:] or 1000)
+# 			
+# 			isClosed = fields[6].get_text().strip() == "Closed"
+# 			if not isClosed:
+# 				log.log("Class %s is now open!" % str(classNumber))
+# 				for subscription in Subscription.objects.filter(theclass=theclass, active=True):
+# 					log.log("Sending subscription %s." % str(subscription))
+# 					subscription.sendNotification()
+# 					subscription.active = False
+# 					subscription.save()
+# 					
+# 			theclass.isClosed = isClosed
+# 			
+# 			theclass.save()
 				
 def scrape():	
 	# Gets the main page of all classes
-	url = "https://registrar.princeton.edu/course-offerings/search_results.xml?term=1134"
-# 	url = 'http://princetonpounce.com/static/course_offerings_test.html'
-
+	url = "https://registrar.princeton.edu/course-offerings/search_results.xml?submit=Search&term=1142"
+	
 	html = urllib2.urlopen(url).read()
 	soup = BeautifulSoup(html)
 
@@ -68,24 +66,26 @@ def scrape():
 			enroll = int(fields[8].get_text().strip())
 			closed = fields[10].get_text().strip() == "Closed"
 			
-			entry, created = Entry.objects.get_or_create(courseNumber=courseNumber, section=section)
- 			if created or enroll != entry.totalEnroll or closed != entry.totalClosed:
-				entry.totalEnroll = enroll
-				entry.totalClosed = closed
-				entry.save()
-
-				course, created = Course.objects.get_or_create(number=courseNumber)
-				course.code = ' / '.join([code.strip().replace("  ", " ") for code in fields[1].text.split('\n \n')])
-				course.title = fields[2].text.strip()
-				course.save()
- 				updateCourse(course)
+			print courseNumber
+			
+# 			entry, created = Entry.objects.get_or_create(courseNumber=courseNumber, section=section)
+#  			if created or enroll != entry.totalEnroll or closed != entry.totalClosed:
+# 				entry.totalEnroll = enroll
+# 				entry.totalClosed = closed
+# 				entry.save()
+# 
+# 				course, created = Course.objects.get_or_create(number=courseNumber)
+# 				course.code = ' / '.join([code.strip().replace("  ", " ") for code in fields[1].text.split('\n \n')])
+# 				course.title = fields[2].text.strip()
+# 				course.save()
+#  				updateCourse(course)
+#  	
+#  	list = CoursesList.objects.all()[0]
+#  	list.cache()
  	
- 	list = CoursesList.objects.all()[0]
- 	list.cache()
- 	
-log.log('Running update.py')
- 
-if (len(CoursesList.objects.all()) == 0):
-	CoursesList().save()
+# log.log('Running update.py')
+#  
+# if (len(CoursesList.objects.all()) == 0):
+# 	CoursesList().save()
 
 scrape()
