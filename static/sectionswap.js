8ecc46e1875fc799778f5bab683b0ee8ea8a1470
Lucas Mayer <lucasjcmayer@gmail.com>
2013-03-30 14:11:00 -0400

    Fake users implemented

--- static/sectionswap.js
+++ static/sectionswap.js
@@ -31,33 +31,46 @@ var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'];
 $('body').css({'background-image': 'url(static/bg/' + images[Math.floor(Math.random() * images.length)] + ')'});
 
 
+function get_user_query() {
+    name = 'netid';
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if(results == null)
+	return "";
+    else
+	return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 // Executed on document load 
 $(document).ready(function(){
-
+    
     $('#container').fadeIn();
-
-	$.getJSON("courses", function(courses){
-		_.each(courses, function(course){
-			var el = $('<option value="' + course['number'] + '"></option>').text(course['code']);
-			$("#section-select").append(el);
+    
+    $.getJSON("courses", function(courses){
+	    _.each(courses, function(course){
+		    var el = $('<option value="' + course['number'] + '"></option>').text(course['code']);
+		    $("#section-select").append(el);
 		});
-		
-	$("#section-select").change(function(){
-		var selectedNumber = $("#section-select").val();
-		var selectedCourse = _.find(courses, function(course){return selectedNumber == course.number;});
-		setSectionOptions(selectedCourse.sections);
+	    
+	    $("#section-select").change(function(){
+	    var selectedNumber = $("#section-select").val();
+	    var selectedCourse = _.find(courses, function(course){return selectedNumber == course.number;});
+	    setSectionOptions(selectedCourse.sections);
 	});
-});
-
-
-	$("#submit").click(function(){
-		var courseNumber = $("#section-select").val();	
-		var haveNumber = $(".have-option:checked").val();
-		var wantNumber = _.map($(".want-option:checked"), function(w){return $(w).val()});
-		var user = "jmcohen";
-		var url = "swaprequest?course=" + courseNumber + "&have=" + haveNumber + "&want=" + wantNumber + "&user=" + user;
-		window.location.href = url;
 	});
-	
-	$("#section-select").select2();
+    
+    
+    $("#submit").click(function(){
+	var courseNumber = $("#section-select").val();	
+	var haveNumber = $(".have-option:checked").val();
+	var wantNumber = _.map($(".want-option:checked"), function(w){return $(w).val()});
+	var user = get_user_query();
+	user = user == "" ? 'jmcohen' : user;
+	var url = "swaprequest?course=" + courseNumber + "&have=" + haveNumber + "&want=" + wantNumber + "&user=" + user;
+	window.location.href = url;
+    });
+    
+    $("#section-select").select2();
 });
